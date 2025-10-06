a798c3eed031359043215be48cab3f918377f980
Isabella Cuchara <isabella.cuchara@escola.pr.gov.br>
2025-10-06 14:06:32 +0000

    salvar

---
+++ Script.js
@@ -0,0 +1,29 @@
+// Seletores do DOM
+const taskForm = document.getElementById('taskForm');
+const taskInput = document.getElementById('taskInput');
+const taskList = document.getElementById('taskList');
+
+// Função que cria um <li> com texto e botão "Remover"
+function createTaskItem(text) {
+  const li = document.createElement('li');
+  const span = document.createElement('li');
+  span.textContent = text;
+  const deleteBtn = document.createElement('button');
+  deleteBtn.textContent = 'Remover';
+  deleteBtn.className = 'delete-btn';
+  deleteBtn.type = 'button';
+  li.appendChild(span);
+  li.appendChild(deleteBtn);
+  return li;
+}
+
+// Captura do submit 
+taskForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const text = taskInput.ariaValueMax.trim();
+  if (!text) return;
+  const li = createTaskItem(text);
+  taskList.appendChild(li);
+  taskInput.value = '';
+  taskInput.focus();
+});
